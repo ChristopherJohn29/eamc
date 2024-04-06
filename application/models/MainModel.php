f6f4a9fc2b95330c20c4f45c74ea9bcd29da51e4
Christopher John Gamo <christopherjohngamo@gmail.com>
2024-04-06 22:22:58 +0800

    update

--- application/models/MainModel.php
+++ application/models/MainModel.php
@@ -230,18 +230,18 @@ class MainModel extends CI_Model {
         return $result;
     }
 
-    public function getCarBYID($id) {
-        $this->db = $this->load->database('default', true);
-
+    public function getCarBYID($id){
         $this->db->select('car.*, source_car.source_name AS source_name, division.div_name AS division, department.dep_name AS department');
         $this->db->from('car');
         $this->db->join('source_car', 'source_car.id = car.source', 'left');
         $this->db->join('division', 'division.id = car.issued_by', 'left');
         $this->db->join('department', 'department.id = car.issued_to', 'left');
-        $this->db->where('car.id', $id); // Corrected the column name here
+        $this->db->where('car.id', $id);
+    
+
         $query = $this->db->get();
         $result = $query->result_array();
-    
+
         return $result;
     }
 
