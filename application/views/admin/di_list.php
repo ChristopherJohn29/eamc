60e462cb558fc75d26c4920f896971868210d8cf
Christopher John Gamo <christopherjohngamo@gmail.com>
2024-09-18 22:33:26 +0800

    update process tracker

--- application/views/admin/di_list.php
+++ application/views/admin/di_list.php
@@ -451,11 +451,16 @@
             }         
             ?>
 
+         <?php 
 
+         if ($this->session->role != 'admin') {
+         ?>
                <div class="mt-2 mb-2" style="text-align: right;">
                   <button type="button" class="btn btn-primary waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#add-di"><i class="fas fa-plus"></i> New Documented Information</button>
                </div>
-
+               <?php
+               }
+               ?>
          </div>
       </div>
       <div class="row">
